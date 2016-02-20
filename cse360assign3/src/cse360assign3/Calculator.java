955410105e44c53b16b8080614ebd957119b518e
TonysPC <tonyjulin@gmail.com>
2016-02-20 00:11:02 -0700

    Assignment3 version2

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,59 +1,63 @@
 package cse360assign3;
 
 public class Calculator {
-
+	
 	private int total;
 	
-	public Calculator () {
-		total = 0;  // not needed - included for clarity
+	public Calculator(){
+		total = 0;	//not needed. included for clarity
 	}
 	
 	/**
 	 * returns the correct total
 	 * @param none
 	 */
-	public int getTotal () {
-		return 0;
+	public int getTotal(){
+		return total;
 	}
 	
 	/**
 	 * adds the parameter value to the total
 	 * @param value
 	 */
-	public void add (int value) {
-		
+	public void add(int value){
+		total = value + total;
 	}
 	
 	/**
 	 * subtracts the parameter value from the total
 	 * @param value
 	 */
-	public void subtract (int value) {
-		
+	public void subtract(int value){
+		total = total - value;
 	}
 	
 	/**
 	 * multiplies the total by the parameter value 
 	 * @param value
 	 */
-	public void multiply (int value) {
-		
+	public void multiply(int value){
+		total = total * value;
 	}
 	
 	/**
 	 * divides the total by the parameter value 
 	 * @param value
 	 */
-	public void divide (int value) {
-		
-		
+	public void divide(int value){
+		if(value == 0)
+			total = 0;
+		else
+			total = total/value;
 	}
 	
 	/**
 	 * returns an empty string ""
 	 * @param none
+	 * 
 	 */
-	public String getHistory () {
-		return "";
+	public String getHistory(){
+		return "";	//empty string 
+		
 	}
 }
--- cse360assign3/src/cse360assign3/CalculatorTest.java
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -14,32 +14,78 @@ public class CalculatorTest {
 	
 	@Test
 	public void testTotal(){
-		fail();
+		//fail();
+		Calculator calc = new Calculator();
+		assertEquals(0, calc.getTotal());	//initial condition should be 0
+		
 	}
 	
 	@Test
 	public void testAdd(){
-		fail();
+		//fail();
+		Calculator calc = new Calculator();
+		calc.add(4);
+		calc.add(3);
+		calc.add(2);
+		assertEquals(9, calc.getTotal());
+		calc.add(20);
+		calc.add(-4);
+		calc.add(-5);
+		assertEquals(20, calc.getTotal());
 	}
 	
 	@Test
 	public void testSubtract(){
-		fail();
+		//fail();
+		Calculator calc = new Calculator();
+		calc.subtract(3);
+		calc.subtract(4);
+		assertEquals(-7, calc.getTotal());
+		calc.subtract(-3);
+		calc.subtract(-4);
+		assertEquals(0, calc.getTotal());
+		calc.subtract(-20);
+		calc.subtract(-8);
+		calc.subtract(12);
+		assertEquals(16, calc.getTotal());
 	}
 	
 	@Test
 	public void testMultiply(){
-		fail();
+		//fail();
+		Calculator calc = new Calculator();
+		calc.multiply(6);
+		assertEquals(0, calc.getTotal());
+		calc.add(3);
+		calc.multiply(3);
+		assertEquals(9, calc.getTotal());
+		calc.multiply(-2);
+		assertEquals(-18, calc.getTotal());
 	}
 	
 	@Test
 	public void testDivide(){
-		fail();
+		//fail();
+		Calculator calc = new Calculator();
+		calc.divide(0);
+		assertEquals(0, calc.getTotal());
+		calc.add(20);
+		calc.divide(0);
+		assertEquals(0, calc.getTotal());
+		calc.add(48);
+		calc.divide(6);
+		assertEquals(8, calc.getTotal());
+		calc.divide(2);
+		assertEquals(4, calc.getTotal());
+		calc.divide(-1);
+		assertEquals(-4, calc.getTotal());
 	}
 	
 	@Test
 	public void getHistory(){
-		fail();
+	//fail();
+	Calculator calc = new Calculator();
+	assertEquals("", calc.getHistory());
 	}
 	
 }
