3d0e063cda9c41a2fba46795a0c74c057fed0884
TonysPC <tonyjulin@gmail.com>
2016-02-19 23:56:10 -0700

    Assignment3 version1

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,59 @@
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * returns the correct total
+	 * @param none
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds the parameter value to the total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtracts the parameter value from the total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiplies the total by the parameter value 
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divides the total by the parameter value 
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+		
+	}
+	
+	/**
+	 * returns an empty string ""
+	 * @param none
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ cse360assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,45 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void testCalculator(){
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+	
+	@Test
+	public void testTotal(){
+		fail();
+	}
+	
+	@Test
+	public void testAdd(){
+		fail();
+	}
+	
+	@Test
+	public void testSubtract(){
+		fail();
+	}
+	
+	@Test
+	public void testMultiply(){
+		fail();
+	}
+	
+	@Test
+	public void testDivide(){
+		fail();
+	}
+	
+	@Test
+	public void getHistory(){
+		fail();
+	}
+	
+}
